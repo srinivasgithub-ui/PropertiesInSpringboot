0496db79e4f10e03e011a6f5ca832b885413ff1c
srinivas <srinivas@example.com>
2020-03-09 23:26:58 +0530

    spacesolve

--- src/main/java/com/springboot/microservice/application/JsonFileGenerator.java
+++ src/main/java/com/springboot/microservice/application/JsonFileGenerator.java
@@ -23,20 +23,17 @@ public class JsonFileGenerator {
 
     jsonarray.add(jsonObjectone);
 
-
     JSONObject jsonObjecttwo = new JSONObject();
     jsonObjecttwo.put("sid","2");
     jsonObjecttwo.put("mid","22");
     jsonarray.add(jsonObjecttwo);
-
 
     JSONObject jsonObjectthree = new JSONObject();
     jsonObjectthree.put("sid","3");
     jsonObjectthree.put("mid","333");
     jsonarray.add(jsonObjectthree);
 
-
-        System.out.println(jsonarray);
+    System.out.println(jsonarray);
 
 }
 
